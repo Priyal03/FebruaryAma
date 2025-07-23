15d3a785289f717a9523af3402ec2b8071d303f2
Priyal Patel <priyalpatel94@gmail.com>
2025-07-23 21:26:09 +0530

    Time: 166 ms (27.2%), Space: 78.1 MB (67.15%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,26 +1,56 @@
-from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
+        self.hashmap = {}
         self.cap = capacity
+        self.left = LinkedNode(0,0)
+        self.right=LinkedNode(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        if key in self.hashmap:
+            self.remove(self.hashmap[key])
+            self.insert(self.hashmap[key])
+            return self.hashmap[key].value
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-            
-        self.cache[key]=value
-        if len(self.cache) > self.cap:
-            self.cache.popitem(last=False)
+        if key in self.hashmap:
+            self.remove(self.hashmap[key])
+
+        self.hashmap[key]=LinkedNode(key,value)
+        self.insert(self.hashmap[key])
+
+        if len(self.hashmap)>self.cap:
+            lru_node =self.left.next 
+            self.remove(lru_node)
+            del self.hashmap[lru_node.key]
+
+    def insert(self,node):
+        prev = self.right.prev
+        nxt = self.right
+
+        prev.next = node
+        nxt.prev=node
+        node.prev = prev
+        node.next = nxt
+
+    def remove(self,node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+class LinkedNode():
+    def __init__(self, key,value):
+        self.key=key
+        self.value=value
+        self.next=None
+        self.prev=None
