686a6fe26a3671e3808b6cb0bc356e4c87ebb7f5
Priyal Patel <priyalpatel94@gmail.com>
2025-08-17 13:15:27 +0530

    Time: 4 ms (70.44%), Space: 19.2 MB (97.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,28 @@
+# Every time we decrease count, it’s like canceling one occurrence of the candidate with one occurrence of a different number.
+
+# Since the majority element occurs more than all others combined, it cannot be fully canceled out.
+
+# So at the end, the candidate must be the majority element.
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        
+        candidate = 0
+        count = 0
 
-        freq = Counter(nums)
-        half = len(nums) / 2
+        for num in nums:
 
-        for digit, count in freq.items():
-            
-            if count > half:
-                return digit
+            if count == 0:
+                candidate = num
 
-        return -1
+            if num==candidate:
+                count += 1
+            else:
+                count -=1
 
-# O(N) and O(N)
+            # print(str(num) +' hey ' + str(candidate))
+            # print('count '+str(count))
+
+        return candidate
+
+# Boyer-Moore is the optimal one (O(N), O(1)) → most interviewers expect you to reach it.
